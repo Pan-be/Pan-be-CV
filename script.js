41ce0b527ef4a65f3aa6614fac7957e8b1044e9e
Konrad <konradbieniek@gmail.com>
2022-12-07 22:49:21 +0100

    finished responsive features

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const links = document.querySelectorAll("a")
+// const dots = document.querySelector(".dots")
 
 const linksArr = Array.from(links)
 
@@ -6,4 +7,12 @@ linksArr.map((link) => {
 	link.setAttribute("target", "_blank")
 })
 
-console.log(linksArr)
+// dots.addEventListener("mouseover", () => {
+// 	dots.classList.remove("dots")
+// 	dots.classList.add("dots-hover")
+// })
+
+// dots.addEventListener("mouseout", () => {
+// 	dots.classList.remove("dots-hover")
+// 	dots.classList.add("dots")
+// })
