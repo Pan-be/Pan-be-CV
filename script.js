1d20f37878f8b929511a557b449c94fbed32c4f9
Konrad Bieniek <konradbieniek@gmail.com>
2024-04-09 17:48:46 +0200

    upgrade professional exp

--- script.js
+++ script.js
@@ -1,18 +1,48 @@
-const links = document.querySelectorAll("a")
-// const dots = document.querySelector(".dots")
+const links = document.querySelectorAll("a");
+const firstBtn = document.getElementById("first_work-btn");
+const secBtn = document.getElementById("second_work-btn");
+const thirdBtn = document.getElementById("third_work-btn");
+const firstDescr = document.getElementById("pos1_description");
+const secDescr = document.getElementById("pos2_description");
+const thirdDescr = document.getElementById("pos3_description");
+const firstBtnContent = document.querySelector("#first_work-btn > p")
+const secondBtnContent = document.querySelector("#second_work-btn > p")
+const thirdBtnContent = document.querySelector("#third_work-btn > p")
 
-const linksArr = Array.from(links)
+const linksArr = Array.from(links);
 
-linksArr.map((link) => {
-	link.setAttribute("target", "_blank")
-})
+linksArr.forEach((link) => {
+	link.setAttribute("target", "_blank");
+});
 
-// dots.addEventListener("mouseover", () => {
-// 	dots.classList.remove("dots")
-// 	dots.classList.add("dots-hover")
-// })
 
-// dots.addEventListener("mouseout", () => {
-// 	dots.classList.remove("dots-hover")
-// 	dots.classList.add("dots")
-// })
+const triggerText = (el, btnText) => {
+	const computedStyle = window.getComputedStyle(el);
+	const isDisplayed = computedStyle.getPropertyValue("display") === "block";
+
+	if (isDisplayed) {
+		el.style.opacity = "0";
+		setTimeout(() => {
+			btnText.textContent = "(read more...)"
+			el.style.display = "none";
+		}, 300);
+	} else {
+		el.style.display = "block";
+		setTimeout(() => {
+			btnText.textContent = "(read less...)"
+			el.style.opacity = "1";
+		}, 10);
+	}
+};
+
+firstBtn.addEventListener("click", () => {
+	triggerText(firstDescr, firstBtnContent);
+});
+
+secBtn.addEventListener("click", () => {
+	triggerText(secDescr, secondBtnContent);
+});
+
+thirdBtn.addEventListener("click", () => {
+	triggerText(thirdDescr, thirdBtnContent);
+});
